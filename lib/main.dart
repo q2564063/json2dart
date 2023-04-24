83a2e965bfb83f0247ae619e5d223195680f8bf4
zzq <>
2023-04-24 15:02:23 +0800

    优化缩放

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,6 @@ import 'package:flutter/services.dart';
 import 'package:json2dart/generator.dart';
 import 'package:json2dart/my_config.dart';
 import 'package:json2dart/storage.dart';
-import 'dart:html' as html;
 
 void main() {
   runApp(const MyApp());
@@ -21,15 +20,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'json2dart',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'dart model generator'),
@@ -39,15 +29,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -76,6 +57,10 @@ class _MyHomePageState extends State<MyHomePage> {
     });
     _controllerClassName.addListener(() {
       refreshData();
+    });
+
+    window.onResize.listen((event) {
+      setState(() {});
     });
   }
 
@@ -145,13 +130,13 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Padding(
+      body: Container(
         padding: const EdgeInsets.all(30.0),
+        height: MediaQuery.of(context).size.height,
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
           children: [
-            //在这里输入className
             Row(
               children: [
                 const Text("类名："),
@@ -164,80 +149,85 @@ class _MyHomePageState extends State<MyHomePage> {
               ],
             ),
             Padding(
-              padding: EdgeInsets.symmetric(vertical: 20),
+              padding: const EdgeInsets.symmetric(vertical: 20),
               child: Text('下载文件名：$downloadFileName'),
             ),
-
-            Column(
-              children: [
-                Row(
-                  children: [
-                    //显示textarea
-                    Expanded(
-                      child: TextField(
-                        controller: _controllerLeft,
-                        maxLines: 30,
-                        decoration: const InputDecoration(
-                          border: OutlineInputBorder(),
-                          hintText: '请输入Json',
+            SizedBox(
+              height: MediaQuery.of(context).size.height / 2,
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      children: [
+                        const Text("json"),
+                        Expanded(
+                          child: TextField(
+                            controller: _controllerLeft,
+                            maxLines: 1000,
+                            decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                            ),
+                          ),
                         ),
-                      ),
-                    ),
-                    const SizedBox(width: 20),
-                    Expanded(
-                      child: TextField(
-                        controller: _controllerRight,
-                        maxLines: 30,
-                        decoration: const InputDecoration(
-                          border: OutlineInputBorder(),
-                          hintText: '生成的代码',
+                        SizedBox(
+                          height: 30,
                         ),
-                      ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            SizedBox(
+                              width: 100,
+                              height: 40,
+                              child: ElevatedButton(
+                                  onPressed: formatAction,
+                                  child: const Text('格式化')),
+                            ),
+                          ],
+                        ),
+                      ],
                     ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Expanded(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          SizedBox(
-                            width: 100,
-                            height: 40,
-                            child: ElevatedButton(
-                                onPressed: formatAction,
-                                child: const Text('格式化')),
+                  ),
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  Expanded(
+                    child: Column(
+                      children: [
+                        const Text("dart"),
+                        Expanded(
+                          child: TextField(
+                            controller: _controllerRight,
+                            maxLines: 1000,
+                            decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                            ),
                           ),
-                        ],
-                      ),
+                        ),
+                        const SizedBox(
+                          height: 30,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            SizedBox(
+                              width: 100,
+                              height: 40,
+                              child: ElevatedButton(
+                                  onPressed: copyAction, child: Text('复制')),
+                            ),
+                            SizedBox(
+                              width: 100,
+                              height: 40,
+                              child: ElevatedButton(
+                                  onPressed: downloadFile, child: Text('下载文件')),
+                            ),
+                          ],
+                        ),
+                      ],
                     ),
-                    Expanded(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          SizedBox(
-                            width: 100,
-                            height: 40,
-                            child: ElevatedButton(
-                                onPressed: copyAction, child: Text('复制')),
-                          ),
-                          SizedBox(
-                            width: 100,
-                            height: 40,
-                            child: ElevatedButton(
-                                onPressed: downloadFile, child: Text('下载文件')),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ],
+                  ),
+                ],
+              ),
             ),
           ],
         ),
@@ -264,21 +254,21 @@ class _MyHomePageState extends State<MyHomePage> {
     var dartFileName = ("${generator.fileName}.dart");
     var content = _controllerRight.text;
     // 将文件内容写入一个Blob对象中
-    final blob = html.Blob([content]);
+    final blob = Blob([content]);
 
     // 创建一个URL对象，用于下载
-    final url = html.Url.createObjectUrlFromBlob(blob);
+    final url = Url.createObjectUrlFromBlob(blob);
 
     // 创建一个anchor元素，设置下载属性和href链接
-    final link = html.document.createElement('a') as html.AnchorElement
+    final link = document.createElement('a') as AnchorElement
       ..href = url
       ..download = dartFileName;
 
     // 将anchor元素添加到DOM中并模拟点击下载
-    html.document.body?.append(link);
+    document.body?.append(link);
     link.click();
 
     // 释放URL对象
-    html.Url.revokeObjectUrl(url);
+    Url.revokeObjectUrl(url);
   }
 }
